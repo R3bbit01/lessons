a6a0bfbdcb40a83bbf13e274fe428bfd92a2ffd3
R3bbit01 <langston.greggory@Gmail.com>
2022-02-20 21:20:21 -0800

    commit

---
+++ Lesson3/lesson3.py
@@ -0,0 +1,16 @@
+num1 = float(input("What is the price of a Child's meal? " ))
+num2 = float(input("What is the price of an adult's mean? "))
+num3 = int(input("How many children are there? "))
+num4 = int(input("How many adults are there? " ))
+num5 = float(input("What is the sales tax rate? "))
+print()
+
+sum = num1 * num3 + num2 * num4
+print("Subtotal: $", sum)
+print("Sales Tax: $", num5 * sum / 100)
+print("Total: $", num5 * sum / 100 + sum)
+print()
+amount = int(input("What is the payment amount:$ "))
+float = amount - (num5 * sum / 100 + sum)
+format_float = "{:.2f}".format(float)
+print("Total: $",format_float)
