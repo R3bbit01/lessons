1614b2b7f306c7648c10c1d1d71d1536415b4313
R3bbit01 <langston.greggory@Gmail.com>
2022-02-27 20:09:12 -0800

    Update

--- Book/age.py
+++ Book/age.py
@@ -6,21 +6,22 @@ def subtract(a, b):
     print(f"SUBTRACTING {a} - {b}")
     return a - b
 
-    def multiply(a, b):
-        print(f"MULTIPLYING {a} * {b}")
-        return a * b
+def multiply(a, b):
+    print(f"MULTIPLYING {a} * {b}")
+    return a * b
 
-        def divide (a, b):
-            print(f"DIVIDING {a} / {b}")
+def divide (a, b):
+    print(f"DIVIDING {a} / {b}")
+    return a / b
 
-            print("Let's do some math with just functions")
+print("Let's do some math with just functions")
 
-            age = add(30, 5)
-            height = subtract(78, 4)
-            weight = multiply(90, 2)
-            iq = divide(100, 2)
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
 
-            print(f"Age: {age}. height: {height}, Weight: {weight}, IQ: {iq, 2}")
+print(f"Age: {age}. height: {height}, Weight: {weight}, IQ: {iq, 2}")
 
         
             
