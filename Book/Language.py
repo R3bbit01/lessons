3d6d8f2a0e9bec2d60b1df61a705886fce5420b8
R3bbit01 <langston.greggory@Gmail.com>
2022-02-27 20:12:59 -0800

    fixed it

---
+++ Book/Language.py
@@ -0,0 +1,14 @@
+import sys
+script, encoding, error = sys.argv
+def main(language_file.readline():
+
+if line:
+    print(line, encoding, errors)
+    return main(language_file, encoding, errors)
+
+    def print_line(line, encoding, errors):
+        next_lang = line.strip()
+        raw_bytes = next_lang.encode(encoding, errors=errors)
+        cooked_striing = raw_bytes.decode(encoding = "utf=8")
+
+        main(language, encoding, error)
