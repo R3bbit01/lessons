bc584d4817355570ca5d8652af45609ddf1f5b56
R3bbit01 <langston.greggory@Gmail.com>
2022-03-06 21:06:29 -0800

    update

--- Book/Language.py
+++ Book/Language.py
@@ -1,14 +1,24 @@
-import sys
-script, encoding, error = sys.argv
-def main(language_file.readline():
+# import sys
+# print(sys.argv)
+# script, encoding, error = sys.argv
 
-if line:
-    print(line, encoding, errors)
-    return main(language_file, encoding, errors)
 
-    def print_line(line, encoding, errors):
-        next_lang = line.strip()
-        raw_bytes = next_lang.encode(encoding, errors=errors)
-        cooked_striing = raw_bytes.decode(encoding = "utf=8")
+# def main(language_file, encoding, errors):
+#     line=language_file.readline()
 
-        main(language, encoding, error)
+#     if line:
+#         print(line, encoding, errors)
+#         return main(language_file, encoding, errors)
+
+# def print_line(line, encoding, errors):
+#     next_lang = line.strip()
+#     raw_bytes = next_lang.encode(encoding, errors=errors)
+#     cooked_string = raw_bytes.decode(encoding, errors=errors)
+#     print(raw_bytes,"<===>", cooked_string)
+
+# language = open("languages.txt", encoding="utf-8")
+# main(language, encoding, error)
+
+m = b"\xe4\xb8\xad\xe6\x96\x87"
+print(m)
+print(m.decode("utf-8"))
