68909e31bdc06265d6bb37512c2955eefdbcea04
R3bbit01 <langston.greggory@Gmail.com>
2022-03-07 20:55:59 -0800

    Update

---
+++ Book/Numbers.py
@@ -0,0 +1,12 @@
+number = 1
+#keep looping as long as the number is less than 10
+while number <= 5:
+    #Display the number
+    print(f"The number is {number}")
+
+    #Update the number to be one more than it was
+    number = number + 1
+    
+    print("Finished with the loop")
+    
+    
