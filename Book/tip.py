5dfd7153e1ce66180c77fdc90e8cd93cba313245
R3bbit01 <langston.greggory@Gmail.com>
2022-03-07 20:17:33 -0800

    update

--- Book/tip.py
+++
@@ -1,9 +0,0 @@
-tip = float(input("What is the tip amount? "))
-
-while tip < 0:
-    print("Sorry, the tip cannot be negative")
-    tip = float(input("What is the tip amount? "))
-#jump back up to line 3
-
-
-    print(f"You have tipped an amount of ${tip:.2f}")
