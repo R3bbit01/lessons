7a6af1f1956e41dc4693c8a69f97a8cda6ac4c20
R3bbit01 <langston.greggory@Gmail.com>
2022-03-13 19:12:34 -0700

    marc's wacko internet changes

--- Lesson7/Pictures/ImageMerge.py
+++ Lesson7/Pictures/ImageMerge.py
@@ -8,6 +8,7 @@ from PIL import Image
 #original image does not get affected
 #Img = Img.copy()
 img = Image.open("beach.jpg")
+img2 = Image.open("cactus.jpg")
 #Img = Img.copy()
 
 #Paste image giving dimensions
@@ -24,14 +25,19 @@ img = Image.open("beach.jpg")
 image_new = Image.new("RGB", img.size)
 pixels_new = image_new.load()
 pixels_original = img.load()
+pixels_overlay = img2.load()
 size = img.size
 print(img.size)
 width = size[0]
 height = size[1]
+that_value_on_pixel_0_0 = img2[0,0]
 for x in range(width):
     for y in range(height):
         r, g, b = pixels_original[x, y]
-        r_new = r + 50
+        r2, g2, b2 = pixels_overlay[x,y]
+        if r2 != that_value_on_pixel_0_0:
+            r = r2
+        r_new = r
         pixels_new[x, y] = (r_new, g, b)
 image_new.show()
 # print(r, g, b)
