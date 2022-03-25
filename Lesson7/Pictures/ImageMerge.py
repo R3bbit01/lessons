c87e207edada03098dae9b5ece45f1631034deba
R3bbit01 <langston.greggory@Gmail.com>
2022-03-25 14:11:09 -0700

    update

--- Lesson7/Pictures/ImageMerge.py
+++ Lesson7/Pictures/ImageMerge.py
@@ -11,13 +11,11 @@ size = img.size
 print(img.size)
 width = size[0]
 height = size[1]
-that_value_on_pixel_0_0 = img2[0,0]
 for x in range(width):
     for y in range(height):
         r, g, b = pixels_original[x, y]
         r2, g2, b2 = pixels_overlay[x, y]
-        if r2 != that_value_on_pixel_0_0:
-            r = r2
+        r = r2
         r_new = r
         pixels_new[x, y] = (r_new, g, b)
 image_new.show()
